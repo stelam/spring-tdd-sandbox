de6ac9ffcdac009ad60eabad87698691ba8a4e46
Stéphane Lam <stelam@gmail.com>
2017-10-27 07:14:54 -0400

    test hook 2

--- src/test/java/com/dev/spring/EchoIT.java
+++ src/test/java/com/dev/spring/EchoIT.java
@@ -10,6 +10,6 @@ public class EchoIT {
 
 	@Test
 	public void firstEchoTest() {
-		get("/echo/hello").then().assertThat().body("message", equalTo("hello"));
+		get("/echo/hello").then().assertThat().body("message", equalTo("hell"));
 	}
 }
